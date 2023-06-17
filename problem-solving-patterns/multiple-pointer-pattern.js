75a1848c31c440ffc5eae566957570840560299c
Osifo John <osifojohntec@gmail.com>
2023-06-17 04:34:28 +0100

    multiple pointer pattern better implementation

--- problem-solving-patterns/multiple-pointer-pattern.js
+++ problem-solving-patterns/multiple-pointer-pattern.js
@@ -11,13 +11,32 @@
 //  Naive approach
 // Time complexity - O(N^2), Space complexity- O(1)
 
+// function sumZero(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[i] + arr[j] === 0) {
+//         console.log(arr[i], arr[j]);
+//         return [arr[i], arr[j]];
+//       }
+//     }
+//   }
+// }
+// sumZero([-4, -3, -2, -1, 0, 1, 2, 5]);
+
+//  A better approach
+// Time complexity - O(N), Space complexity- O(1)
 function sumZero(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] + arr[j] === 0) {
-        console.log(arr[i], arr[j]);
-        return [arr[i], arr[j]];
-      }
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      console.log([arr[left], arr[right]]);
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
     }
   }
 }
