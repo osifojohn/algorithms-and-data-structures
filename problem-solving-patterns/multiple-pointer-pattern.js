8bfc29ef403dc4961a0b42776f8a8b29b295655b
Osifo John <osifojohntec@gmail.com>
2023-06-17 06:05:12 +0100

    implemented countUniqueValues exercise

--- problem-solving-patterns/multiple-pointer-pattern.js
+++ problem-solving-patterns/multiple-pointer-pattern.js
@@ -11,17 +11,17 @@
 //  Naive approach
 // Time complexity - O(N^2), Space complexity- O(1)
 
-// function sumZero(arr) {
-//   for (let i = 0; i < arr.length; i++) {
-//     for (let j = i + 1; j < arr.length; j++) {
-//       if (arr[i] + arr[j] === 0) {
-//         console.log(arr[i], arr[j]);
-//         return [arr[i], arr[j]];
-//       }
-//     }
-//   }
-// }
-// sumZero([-4, -3, -2, -1, 0, 1, 2, 5]);
+function sumZero(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === 0) {
+        console.log(arr[i], arr[j]);
+        return [arr[i], arr[j]];
+      }
+    }
+  }
+}
+sumZero([-4, -3, -2, -1, 0, 1, 2, 5]);
 
 //  A better approach
 // Time complexity - O(N), Space complexity- O(1)
@@ -41,3 +41,20 @@ function sumZero(arr) {
   }
 }
 sumZero([-4, -3, -2, -1, 0, 1, 2, 5]);
+
+function countUniqueValues(arr) {
+  if (arr.length === 0) return 0;
+  let i = 0;
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  console.log(arr);
+  return i + 1;
+}
+countUniqueValues([1, 1, 1, 1, 1, 2]); // 2
+countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]); // 7
+countUniqueValues([]); // 0
+countUniqueValues([-2, -1, -1, 0, 1]); // 4
